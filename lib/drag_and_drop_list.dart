7e964329f20c696801768fa6c054bd5325b74a75
Eymeric Dechelette <hatchchien@protonmail.com>
2023-03-10 14:45:35 +0100

    Smooth animation when list grow up or down

--- lib/drag_and_drop_list.dart
+++ lib/drag_and_drop_list.dart
@@ -99,10 +99,14 @@ class DragAndDropList implements DragAndDropListInterface {
           ? double.infinity
           : params.listWidth - params.listPadding!.horizontal,
       decoration: decoration ?? params.listDecoration,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: verticalAlignment,
-        children: contents,
+      child: AnimatedSize(
+        duration: Duration(milliseconds: params.listSizeAnimationDuration),
+        curve: Curves.easeInOut,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: verticalAlignment,
+          children: contents,
+        ),
       ),
     );
   }
